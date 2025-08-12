b066fba98905acc84373e9db5a2a77d3e555ea3d
DeepanJM <deepan.makan@gmail.com>
2025-08-12 14:20:34 +0200

    Refactor login functionality and improve test case documentation

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,7 @@
 exports.LoginPage = class LoginPage {
 
+    // This class represents the login page and contains methods to interact with it
+    // It uses Playwright's page object model to encapsulate the login functionality
     constructor(page) {
         this.page = page;
         this.username_textbox = page.getByRole('textbox', { name: 'Username' });
@@ -7,20 +9,23 @@ exports.LoginPage = class LoginPage {
         this.login_button = page.getByRole('button', { name: ' Login' });
     }
 
+    // This method navigates to the login page
     async gotoLoginPage() {
         await this.page.goto('https://the-internet.herokuapp.com/login');
     }
 
-    // enterUsername(username) {
-    //     return this.username_textbox.fill(username);
-    // }
-    // enterPassword(password) {
-    //     return this.password_textbox.fill(password);
-    // }
-    // clickLoginButton() {
-    //     return this.login_button.click();
-    // }
+    /*This is an alternative way to implement the login functionality
+    enterUsername(username) {
+        return this.username_textbox.fill(username);
+    }
+    enterPassword(password) {
+        return this.password_textbox.fill(password);
+    }
+    clickLoginButton() {
+        return this.login_button.click();
+    }*/
 
+    // This is the preferred way to implement the login functionality
     async loginInfo(username, password) {
         await this.username_textbox.fill(username);
         await this.password_textbox.fill(password);
--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -3,15 +3,22 @@ import {LoginPage} from '../../pages/login.js';
 
 test('test', async ({ page }) => {
 
+  // This test case demonstrates how to use the LoginPage class to perform a login action
+  // It first navigates to the login page and then fills in the username and password fields
+  // Finally, it clicks the login button to submit the form
+  // The LoginPage class encapsulates the functionality of the login page, making it reusable and maintainable
   const loginPage = new LoginPage(page);
   await loginPage.gotoLoginPage();
   await loginPage.loginInfo('tomsmith', 'SuperSecretPassword!');
 
 
-  // await page.goto('https://the-internet.herokuapp.com/login');
-  // await page.getByRole('textbox', { name: 'Username' }).click();
-  // await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
-  // await page.getByRole('textbox', { name: 'Password' }).click();
-  // await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
-  // await page.getByRole('button', { name: ' Login' }).click();
+  /*
+  This is a simple way of implementing the login functionality
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  */
 });
